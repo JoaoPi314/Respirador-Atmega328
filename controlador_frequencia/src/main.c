1431cd30e51a792486d78c07bc3ffb7a383641f7
João Pedro <joao.melquiades@ee.ufcg.edu.br>
2021-03-16 16:40:26 -0300

    Botões implementados com interrupções

--- controlador_frequencia/src/main.c
+++ controlador_frequencia/src/main.c
@@ -5,38 +5,47 @@
  *---------------------------------------------------------------------	*/
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "libs/animateLed.h"
 #include "libs/mascaras.h"
 
+
+//Variável estática só válida dentro desse arquivo
+static volatile uint8_t FreqRespiracao = 12;
+	
+
+
+ISR(INT0_vect){				//Interrupção externa 0 (PD2)
+	if(FreqRespiracao < 30)	//Enquanto a frequência for menor que 30, incremente
+		FreqRespiracao++;
+}
+
+ISR(INT1_vect){				//Interrupção externa 1 (PD3)
+	if(FreqRespiracao > 5)	//Enquanto a frequência for maior que 5, decremente
+		FreqRespiracao--;
+}
+
+
 int main(void)
 {
-	//Configuração das portas
+	//GPIO
+	//Definição de direção das portas
 	DDRB |= 0b11111111;					//Todos os pinos da porta B serão de saída
-	DDRD |= ~(0b00000011<<0);			//Aciona os pinos 0 e 1 da porta D como entrada
+	DDRD |= ~(0b00001100<<0);			//Aciona os pinos 2 e 3 da porta D como entrada
 	DDRC |= 0b11111111;					//Todos os pinos da porta C serão de saída
-
-	PORTD |= (0b00000011<<0);			//Aciona o pull-up interno para os pinos 0 e 1 da porta D
+	//Inicialização e pull-ups
+	PORTD |= (0b00001100<<0);			//Aciona o pull-up interno para os pinos 0 e 1 da porta D
 	PORTB |= 0b00000000;				//Inicialmente todas as saídas estão NLB em B
-	//Variáveis locais
-	uint8_t FreqRespiracao = 12;		//Respiração normal do ser humano em condições normais
-		
-	PORTC = FreqRespiracao;
+
+	//Interrupções
+	EICRA = 0b00001010;					//Interrupções INT1 e INT0 ativadas na borda de descida
+	EIMSK =	0b00000011;					//Ativa as interrupções INT1 e INT0
+	sei();								//Bit SREG em 1 - Interrupções globais ativadas
+
 	
     while (1) 
     {
-		//Aumentar a frequência tem prioridade caso os dois botôes sejam apertados ao mesmo tempo
-		if(!(PIND & (1<<0))){			//Se o pino 0 da porta D for 0, aumentar a frequência
-			FreqRespiracao ++;
-		} else if(!(PIND & (1<<1))){	//Se o pino 1 da porta D for 0, diminuir a frequência
-			FreqRespiracao --;
-		}
-		
-		//Verifica se a frequência de respiração ultrapassou os limites
-		if(FreqRespiracao < 5)
-			FreqRespiracao ++;
-		else if(FreqRespiracao > 30)
-			FreqRespiracao --;
-		
+			
 		PORTC = FreqRespiracao;
 
 		//Atribuiçãoo da configuração de LEDS à porta B
