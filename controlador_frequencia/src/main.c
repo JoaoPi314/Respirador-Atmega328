241487ef9207aa0095644a53c91b04865ad49ab0
João Pedro <joao.melquiades@ee.ufcg.edu.br>
2021-03-16 18:33:36 -0300

    Adiciona display Nokia 5110 no lugar do display de 7 segmentos para mostrar a frequência

--- controlador_frequencia/src/main.c
+++ controlador_frequencia/src/main.c
@@ -4,10 +4,12 @@
  * Descrição: Arquivo com conjunto de instruções que irão para o ATmega	*
  *---------------------------------------------------------------------	*/
 
+#include <stdlib.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "libs/animateLed.h"
 #include "libs/mascaras.h"
+#include "libs/nokia5110.h"
 
 
 //Variável estática só válida dentro desse arquivo
@@ -28,11 +30,17 @@ ISR(INT1_vect){				//Interrupção externa 1 (PD3)
 
 int main(void)
 {
+
+	//LCD inits
+	nokia_lcd_init();
+	nokia_lcd_clear();
+	nokia_lcd_set_cursor(35,20);
+	nokia_lcd_write_string("resp/min", 1);
+
 	//GPIO
 	//Definição de direção das portas
 	DDRB |= 0b11111111;					//Todos os pinos da porta B serão de saída
 	DDRD |= ~(0b00001100<<0);			//Aciona os pinos 2 e 3 da porta D como entrada
-	DDRC |= 0b11111111;					//Todos os pinos da porta C serão de saída
 	//Inicialização e pull-ups
 	PORTD |= (0b00001100<<0);			//Aciona o pull-up interno para os pinos 0 e 1 da porta D
 	PORTB |= 0b00000000;				//Inicialmente todas as saídas estão NLB em B
@@ -43,15 +51,25 @@ int main(void)
 	sei();								//Bit SREG em 1 - Interrupções globais ativadas
 
 	
+	//Variáveis locais
+	char Freq_str[3];
+
     while (1) 
     {
-			
-		PORTC = FreqRespiracao;
+    	itoa(FreqRespiracao, Freq_str, 10);
+    	//sprintf(Freq_str, "%02d", FreqRespiracao);
+    	if(FreqRespiracao < 10){
+    		nokia_lcd_set_cursor(0, 10);
+    		nokia_lcd_write_string("0", 3);
+			nokia_lcd_set_cursor(16,10);
+    	}
+		else
+			nokia_lcd_set_cursor(0, 10);
+		nokia_lcd_write_string(Freq_str, 3);
+		nokia_lcd_render();
 
 		//Atribuiçãoo da configuração de LEDS à porta B
 		PORTB = animateLed(FreqRespiracao);
-		
-
 		
     }
 }
