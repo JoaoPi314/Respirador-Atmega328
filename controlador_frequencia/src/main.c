9c7fabec5246a5bf78680e10cf7221c434fd0062
João Pedro <joao.melquiades@ee.ufcg.edu.br>
2021-04-05 18:03:21 -0300

    Adds temperature sensor and adds a danger buzzer

--- controlador_frequencia/src/main.c
+++ controlador_frequencia/src/main.c
@@ -14,10 +14,12 @@
 static volatile uint8_t FreqRespiracao = 12;
 static volatile uint32_t FreqCardiaca = 60;
 static volatile uint16_t saturacaoO2 = 0;
+static volatile float temper = 0;
 
 static volatile uint32_t tempo_ms = 0;
 
 static volatile uint8_t ledFlag = 0;
+static volatile uint8_t flagLCD = 0;
 
 static volatile uint16_t t_last = 0;
 static volatile uint32_t last_period = 0;
@@ -45,7 +47,11 @@ int main(void)
 	uint8_t result = 0;
 
     while (1){ 
-  		changeDisplayConfig(displayConfigFlag, FreqRespiracao, FreqCardiaca, saturacaoO2);//Plota o gráfico da frequência x tempo e indica a frequência atual	
+  		
+    	if(flagLCD){
+  			changeDisplayConfig(displayConfigFlag, FreqRespiracao, FreqCardiaca, saturacaoO2, temper);//Plota o gráfico da frequência x tempo e indica a frequência atual	
+    		flagLCD = 0;
+    	}
     	
     	if(ledFlag){
 	    	result = animateLed(FreqRespiracao);				//Chama rotina de animação de leds
@@ -69,7 +75,7 @@ void init_registers(){
 	//GPIO
 	//Definição de direção das portas
 	
-	DDRC |= 0xFE;
+	DDRC |= 0b11111100;
 	DDRB |= 0b11111110;					//Todos os pinos da porta B serão de saída
 	DDRD |= ~(0b10001100<<0);			//Aciona os pinos 2, 3 e 7 da porta D como entrada
 	//Inicialização e pull-ups
@@ -97,7 +103,7 @@ void init_registers(){
 
 
 	ADMUX = 0b01000001;					//VREF ligada, Habilita canal 0
-	//ADCSRA = 0b11101111;				//Habilita AD, habilita interrupção, conversão contínua,
+	ADCSRA = 0b00000111;				//Habilita AD, habilita interrupção, conversão contínua,
 										//prescaler = 128
 	ADCSRB = 0x00;						//Modo de conversão contínua
 	DIDR0 = 0b00111100;					//Pinos PC0 e PC1 como entrada ADC0 e ADC1
@@ -115,10 +121,23 @@ void init_lcd(){
 
 
 ISR(ADC_vect){
-	ADCSRA = 0b00000000;
+	clr_bit(ADCSRA, 7);
+	clr_bit(ADCSRA, 6);
+	clr_bit(ADCSRA, 5);
+	clr_bit(ADCSRA, 3);
+	//ADCSRA = 0b00000000;
 	cpl_bit(PORTD, 0);
 
-	saturacaoO2 = 0.123*ADC;
+	if(tst_bit(ADMUX, 0))
+		saturacaoO2 = 0.123*ADC;
+	else
+		temper = (ADC + 205	)/20.46;
+
+	if(saturacaoO2 > 60 || temper < 35 || temper > 41)
+		if(!(tempo_ms % 200))
+			cpl_bit(PORTD, 5);
+	else
+		clr_bit(PORTD, 5);
 
 }
 
@@ -146,8 +165,17 @@ ISR(TIMER0_COMPA_vect){			//Interrupção por overflow do TC0
 		ledFlag = 1;
 	}
 
-	if(!(tempo_ms % 150))
-		ADCSRA = 0b11101111;
+	if(!(tempo_ms % 150)){
+		cpl_bit(ADMUX, 0);
+		set_bit(ADCSRA, 7);
+		set_bit(ADCSRA, 6);
+		set_bit(ADCSRA, 5);
+		set_bit(ADCSRA, 3);
+	}
+
+	if(!(tempo_ms % 200))
+		flagLCD = 1;
+
 }
 
 ISR(INT0_vect){					//Interrupção externa em PD2
